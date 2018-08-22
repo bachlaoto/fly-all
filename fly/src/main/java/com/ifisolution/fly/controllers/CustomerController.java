d5f73dd75b59f31a255ba7c06838cf4967b5ecf7
bachlaoto <bachlaoto@gmail.com>
2018-08-22 17:41:33 +0700

    update fly

--- fly/src/main/java/com/ifisolution/fly/controllers/CustomerController.java
+++ fly/src/main/java/com/ifisolution/fly/controllers/CustomerController.java
@@ -2,14 +2,25 @@ package com.ifisolution.fly.controllers;
 
 import com.ifisolution.fly.domain.Customer;
 import com.ifisolution.fly.services.CustomerService;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @Controller
-@RequestMapping("admin/customer")
+@RequestMapping("customer")
 public class CustomerController {
+    private final String domain = "http://localhost:8080";
+
     @Autowired
     private CustomerService customerService;
 
@@ -35,9 +46,12 @@ public class CustomerController {
     }
 
     @PostMapping("/customer")
-    public String saveOrUpdate(@ModelAttribute Customer customer ){
-        Customer savedCustomer = customerService.save(customer);
-        return "redirect:/customer/" + savedCustomer.getId() + "/show";
+    public String saveOrUpdate(@ModelAttribute Customer customer) throws IOException {
+//        Customer savedCustomer = customerService.save(customer);
+        saveCustomer(customer);
+
+        return "redirect:/customer/" + customer.getId() + "/show";
+//        return "redirect:/customer";
     }
     @GetMapping(value = {"","/","index"})
     public String getAllCustomer(Model model){
@@ -45,10 +59,62 @@ public class CustomerController {
         return "customer/customers";
     }
 
-    @GetMapping("{id}/delete")
-    public String deleteCustomerById(@PathVariable String id){
-
-        customerService.deleteCustomer(Long.valueOf(id));
+    @GetMapping("/delete/{id}")
+    public String deleteCustomerById(@PathVariable String id) throws IOException {
+        delete(id);
+//        customerService.deleteCustomer(Long.valueOf(id));
         return "redirect:/customer";
+    }
+
+    public boolean saveCustomer(Customer cus) throws IOException {
+        boolean check = true;
+        JSONObject json = new JSONObject();
+        json.put("customerName", cus.getCustomerName());
+        json.put("otherDetails", cus.getOtherDetails());
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            //HttpPost request = new HttpPost(domain+"/api/customer");
+            HttpPost request = new HttpPost(domain + "/customer/new");
+            StringEntity params = new StringEntity(json.toString());
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            httpClient.execute(request);
+            check = true;
+        } catch (Exception ex) {
+            check = false;
+        } finally {
+            httpClient.close();
+        }
+        return check;
+    }
+
+    public boolean delete(String id) throws IOException {
+        boolean check = true;
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        try {
+            //HttpPost request = new HttpPost(domain+"/api/customer");
+            HttpDelete request = new HttpDelete(domain + "/customer/delete/" + id);
+            request.addHeader("content-type", "application/json");
+            httpClient.execute(request);
+        } catch (Exception ex) {
+            check = false;
+        }
+        return check;
+    }
+
+    public boolean showCustomer(Customer cus) {
+        boolean check = true;
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        try {
+            //HttpPost request = new HttpPost(domain+"/api/customer");
+            HttpGet request = new HttpGet(domain + "/customer/get");
+            request.addHeader("content-type", "application/json");
+            httpClient.execute(request);
+        } catch (Exception ex) {
+            check = false;
+        }
+        return check;
     }
 }
